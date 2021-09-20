2e5bd01027e4b438e429c2612fa93c3be0d3fb7b
Vladimir Sorokin <v.d.sorokin@gmail.com>
2021-09-20 16:36:03 +0300

    add rog

---
+++ rog/home.nix
@@ -0,0 +1,63 @@
+{pkgs}:
+{
+  programs.urxvt = {
+    fonts = [ "xft:Terminess Powerline:style=Regular:size=22" ];
+    extraConfig = {
+      letterspace = "-2";
+    };
+  };
+  xsession = {
+    enable = true;
+    pointerCursor = {
+      size = 32;
+    };
+
+    preferStatusNotifierItems = true;
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages: [
+          haskellPackages.taffybar
+        ];
+      config = ./xmonad.hs;
+    };
+
+    initExtra = ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --dpi 144
+    '';
+  };
+  xresources.properties = {
+    "Xft.dpi" = 144;
+  };
+  services = {
+    network-manager-applet = {
+      enable = true;
+    };
+    random-background = {
+      enable = true;
+      imageDirectory = "%h/Dropbox/Photos/Backgrounds";
+      interval = "30min";
+    };
+    status-notifier-watcher = {
+      enable = true;
+    };
+    taffybar = {
+      enable = true;
+    };
+    xembed-sni-proxy = {
+      enable = true;
+    };
+  };
+
+  home.packages = with pkgs; [
+    notify-osd
+  ];
+
+  home.file = {
+    ".config/taffybar/taffybar.hs".source = ./taffybar.hs;
+    ".config/taffybar/taffybar.css".source = ./taffybar.css;
+
+    ".notify-osd".source = ./notify-osd.conf;
+    ".emacs".text = import ../emacs.nix { fontSize = 14; };
+  };
+}
